16a21d07bac6c72fd23607f54cd85f07d1f23dd0
NasrinRose <niso1234542@gmail.com>
2021-11-22 18:22:22 +0100

    Commited-sum+fib

--- src/sumn.c
+++ src/sumn.c
@@ -1,8 +1,15 @@
 #include "sumn.h"
 
 #include<stdio.h>
-
+#include<assert.h>
+/* sum of the first n positive odd numbers.*/
 int sumn (int n)
 {
-    return 0;
+/* pre-condition */
+  assert (n >= 1);
+/* post-condition */
+  if (n > 1)
+    return (n*2-1)+sumn(n-1);
+  else
+    return 1;
 }
